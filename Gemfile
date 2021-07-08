66c941938f92cef52852cb900a683be75d72260d
Troy Russell <troyjruss@gmail.com>
2021-07-08 20:11:28 +0900

    yelp mvp working

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+# Added by me to Gemfile
+gem 'autoprefixer-rails'
+gem 'faker'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.0.4'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+puts 'creating 5 restaurants'
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+end
+puts '5 restaurants created'
